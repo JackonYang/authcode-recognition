66530bc8a94fcc3b15ca8c5214572772d8c4c7f5
Jackon Yang <jiekunyang@gmail.com>
2015-04-27 20:17:24 +0800

    auto_test

--- auto_test.py
+++ auto_test.py
@@ -1,5 +1,12 @@
 # -*- Encoding: utf-8 -*-
+import os
 from parser import recog
+import codecs
+from jinja2 import Environment, FileSystemLoader
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+template_dir = os.path.join(BASE_DIR, "templates")
 
 def test(filenames):
     total = len(filenames)
@@ -13,6 +20,15 @@ def test(filenames):
     return result
 
 
+def disp(data):
+    env = Environment(loader=FileSystemLoader(template_dir))
+    template = env.get_template('index.html')
+
+    filename = 'result.html'
+    with codecs.open(filename, 'w', 'utf8') as f:
+        f.write(template.render(data=data))
+    print 'success! saved in %s' % os.path.abspath(filename)
+
 if __name__ == '__main__':
     import sys
 
@@ -24,4 +40,4 @@ if __name__ == '__main__':
     else:
         filenames = sys.argv[1:]
 
-    print test(filenames)
+    disp(test(filenames))
