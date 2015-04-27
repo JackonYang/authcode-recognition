6dbd10e6416451e65aaf9482c36115a608bd187e
Jackon Yang <jiekunyang@gmail.com>
2015-04-27 20:41:53 +0800

    auto_test.py

--- auto_test.py
+++ auto_test.py
@@ -1,24 +1,12 @@
 # -*- Encoding: utf-8 -*-
 import os
 from parser import recog
+import download
 import codecs
 from jinja2 import Environment, FileSystemLoader
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-template_dir = os.path.join(BASE_DIR, "templates")
-
-def test(filenames):
-    total = len(filenames)
-    result = []
-    for f, i in zip(filenames, range(total)):
-        print 'recognizing. %d / %d | fliename = %s' % (i, total, f)
-        result.append({
-            'result': recog(f),
-            'filename': f,
-            })
-    return result
-
+template_dir = "templates"
 
 def disp(data):
     env = Environment(loader=FileSystemLoader(template_dir))
@@ -27,17 +15,29 @@ def disp(data):
     filename = 'result.html'
     with codecs.open(filename, 'w', 'utf8') as f:
         f.write(template.render(data=data))
-    print 'success! saved in %s' % os.path.abspath(filename)
+    print 'success! saved in %s' % filename
+
+def run_test(url):
+    download.reqs(url, 2)
+
+    filenames = [os.path.join(download.download_dir, f) for f in os.listdir(download.download_dir)]
+    total = len(filenames)
+
+    result = []
+    for f, i in zip(filenames, range(total)):
+        print 'recognizing. %d / %d | fliename = %s' % (i, total, f)
+        result.append({
+            'result': recog(f),
+            'filename': f,
+            })
+    disp(result)
 
 if __name__ == '__main__':
     import sys
 
-    if len(sys.argv) == 1:
-        filename = raw_input(u'picture filename: ')
-        filenames = [filename]
-    elif len(sys.argv) == 2:
-        filenames = [sys.argv[1]]
+    if len(sys.argv) < 2:
+        url = raw_input(u'validation code url: ')
     else:
-        filenames = sys.argv[1:]
+        url = sys.argv[1]
 
-    disp(test(filenames))
+    run_test(url)
